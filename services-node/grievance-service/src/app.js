b572a26509d7bb36b1bde0bca2bcdfd4f4979862
hassan-jamshaid10 <hjamshaid81@gmail.com>
2026-04-19 04:49:00 +0500

    added cors

--- services-node/grievance-service/src/app.js
+++ services-node/grievance-service/src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const grievanceRoutes = require('./routes/grievance.routes');
 
 const app = express();
+
+app.use(
+	cors({
+		origin: true,
+		credentials: true,
+		methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+	})
+);
+app.options('*', cors());
 
 app.use(express.json());
 
